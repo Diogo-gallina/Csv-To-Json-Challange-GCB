4aa938e5a1a8fd5b6fc3577a757b8262ec0dc790
Diogo-gallina <diogogallina20@gmail.com>
2023-06-21 18:06:37 -0300

    feat: create-convertCsvToJson-function

---
+++ js/convert-csv-to-json.js
@@ -0,0 +1,15 @@
+const convertCsvToJson = (csvFile) => {
+    const rows = splitCsvRows(csvFile);
+    const columnName = extractColumnNames(rows[0]);
+    const jsonFile = [];
+  
+    for (let currentRow = 1; currentRow < rows.length; currentRow++) {
+      const row = rows[currentRow].split(',');
+      const objJson = createJsonObject(columnName, row);
+      
+      jsonFile.push(objJson);
+    }
+  
+    return JSON.stringify(jsonFile, null, 2);
+  };
+  
