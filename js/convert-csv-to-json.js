809ec5a9470eaf64b0771f99d87ab9e959f4db47
Diogo-gallina <diogogallina20@gmail.com>
2023-06-22 07:00:54 -0300

    feat: create-processFile-function

--- js/convert-csv-to-json.js
+++ js/convert-csv-to-json.js
@@ -1,33 +1,50 @@
-const convertCsvToJson = (csvFile) => {
-    const rows = splitCsvRows(csvFile);
-    const columnName = extractColumnNames(rows[0]);
-    const jsonFile = [];
-  
-    for (let currentRow = 1; currentRow < rows.length; currentRow++) {
-      const row = rows[currentRow].split(',');
-      const objJson = createJsonObject(columnName, row);
-      
-      jsonFile.push(objJson);
+const fs = require('fs');
+const path = require('path');
+
+const csvFolder = path.join(__dirname, '..', 'csv');
+const jsonFolder = path.join(__dirname, '..', 'json');
+
+const processFiles = () => {
+  fs.readdir(csvFolder, (err, files) => {
+    if (err) {
+      handleError('Error reading CSV folder:', err);
+      return;
     }
-  
-    return JSON.stringify(jsonFile, null, 2);
-  };
-  
-const splitCsvRows = (csvFile) => {
-return csvFile.split('\r\n');
-};
 
-const extractColumnNames = (csvRow) => {
-return csvRow.split(',');
-};
-
-const createJsonObject = (columnNames, rowData) => {
-const objJson = {};
-
-for (let currentData = 0; currentData < rowData.length; currentData++) {
-    objJson[columnNames[currentData]] = rowData[currentData];
+    files.forEach(processFile);
+  });
 }
 
-return objJson;
-};
-  
+const convertCsvToJson = (csvFile) => {
+  const rows = splitCsvRows(csvFile);
+  const columnName = extractColumnNames(rows[0]);
+  const jsonFile = [];
+
+  for (let currentRow = 1; currentRow < rows.length; currentRow++) {
+    const row = rows[currentRow].split(',');
+    const objJson = createJsonObject(columnName, row);
+
+    jsonFile.push(objJson);
+  }
+
+  return JSON.stringify(jsonFile, null, 2);
+}
+
+const splitCsvRows = (csvFile) => {
+  return csvFile.split('\r\n');
+}
+
+const extractColumnNames = (csvRow) => {
+  return csvRow.split(',');
+}
+
+const createJsonObject = (columnNames, rowData) => {
+  const objJson = {};
+
+  for (let currentData = 0; currentData < rowData.length; currentData++) {
+    objJson[columnNames[currentData]] = rowData[currentData];
+  }
+
+  return objJson;
+}
+
